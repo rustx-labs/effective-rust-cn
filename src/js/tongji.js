5323dc1795c78e9d6e5222e93fca53d4eb806d5f
lispking <lispking@qq.com>
2024-04-20 21:03:31 +0800

    Add baidu tongji

---
+++ src/js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2ea8280e6459b30fc19e21785c5c4363";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
